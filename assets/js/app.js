cad91c59785a6bcb01fe503979c6d3ad06aa702b
azharanowar <azharanowar@gmail.com>
2022-09-03 21:24:43 +0600

    Validation for not required field

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,36 +1,58 @@
-document.getElementById("incomeInput").addEventListener('blur', function() {
+document.getElementById("incomeInput").addEventListener('keyup', function() {
     inputFieldValueValidation( "incomeInput", "incomeInputMessage" );
     reuiredFieldsValidation();
-    
 });
 
-document.getElementById("foodCostInput").addEventListener('blur', function() {
+document.getElementById("foodCostInput").addEventListener('keyup', function() {
     inputFieldValueValidation( "foodCostInput", "foodCostInputMessage" );
     reuiredFieldsValidation();
 });
 
-document.getElementById("rentCostInput").addEventListener('blur', function() {
+document.getElementById("rentCostInput").addEventListener('keyup', function() {
     inputFieldValueValidation( "rentCostInput", "rentCostInputMessage" );
     reuiredFieldsValidation();
 });
 
+document.getElementById("clothesCostInput").addEventListener('keyup', function() {
+    inputFieldValueValidation( "clothesCostInput", "clothesCostInputMessage" );
+});
+
+document.getElementById("calculateBtn").addEventListener('click', function() {
+    const totalExpenses = totalExpensesCalculation();
+    console.log(totalExpenses);
+});
 
 
 
+function totalExpensesCalculation() {
+    let totalExpenses = 0;
+    const expensesInputFields = document.getElementsByClassName("expenses-input-fields");
+    for ( const expensesInputField of expensesInputFields ) {
+        if ( expensesInputField.value && !isNaN(expensesInputField.value) ) {
+            totalExpenses += parseInt( expensesInputField.value );
+        }
+    }
+
+    return totalExpenses;
+}
 
 function inputFieldValueValidation( inputFieldId, messageElementId ) {
     const inputField = document.getElementById( inputFieldId );
     const inputFieldValue = inputField.value;
 
-    if ( inputFieldValue === "" ) {
+    if ( inputFieldValue === "" && inputField.classList.contains('required') ) {
         document.getElementById( messageElementId ).innerText = "This field can't be empty!!!";
         document.getElementById( inputFieldId ).style.border = "2px solid #DC3545";
+        document.getElementById("calculateBtn").disabled = true;
+        
     } else if ( isNaN( inputFieldValue ) || inputFieldValue < 0 ) {
         document.getElementById( messageElementId ).innerText = "Input field most be a positive number!!!";
         document.getElementById( inputFieldId ).style.border = "2px solid #DC3545";
+        document.getElementById("calculateBtn").disabled = true;
     } else {
         document.getElementById( messageElementId ).innerText = "";
         document.getElementById( inputFieldId ).style.border = "";
+        document.getElementById("calculateBtn").disabled = false;
     }
 
     return;
@@ -49,9 +71,9 @@ function reuiredFieldsValidation() {
 
     if ( requiredConditionNotMaintained === 0 ) {
         document.getElementById("calculateBtn").disabled = false;
-        return true; // All required field has valid value
+        // return true; // All required field has valid value
     } else {
         document.getElementById("calculateBtn").disabled = true;
-        return false; // Any of required field has error
+        // return false; // Any of required field has error
     }
 }
