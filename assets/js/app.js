77ffe413cadf329a4802215713075fdbc4a36fb8
azharanowar <azharanowar@gmail.com>
2022-09-04 00:43:28 +0600

    Save percentage validation, saving amount update, remaining amount calculation everything is completed

--- assets/js/app.js
+++ assets/js/app.js
@@ -17,9 +17,26 @@ document.getElementById("clothesCostInput").addEventListener('keyup', function()
     inputFieldValueValidation( "clothesCostInput", "clothesCostInputMessage" );
 });
 
+let totalBalanceLeft;
 document.getElementById("calculateBtn").addEventListener('click', function() {
     const totalExpenses = totalExpensesCalculation();
-    const totalBalanceLeft = balanceLeftCalculation( totalExpenses );
+    totalBalanceLeft = balanceLeftCalculation( totalExpenses );
+
+    if ( totalBalanceLeft ) {
+        document.getElementById("savePercentageInput").disabled = false;
+    }
+});
+
+
+
+document.getElementById("savePercentageInput").addEventListener('keyup', function() {
+    savePercantageInputValidation();
+    
+});
+
+document.getElementById("saveBtn").addEventListener('click', function() {
+    const totalSavingAmount = totalSavingAmountCalculation();
+    remainingBalanceCalculation( totalSavingAmount );
 });
 
 
@@ -41,6 +58,7 @@ function balanceLeftCalculation( totalExpenses ) {
     let totalIncomeAmount = parseFloat( incomeInput.value );
     if ( totalIncomeAmount > totalExpenses ) {
         totalIncomeAmount -= totalExpenses;
+        document.getElementById("expensesErrorMessage").innerText = '';
         return document.getElementById("balenceLeftAmount").innerText = totalIncomeAmount;
     } else {
         document.getElementById("expensesErrorMessage").innerText = "Expenses can't be more than your income!!!";
@@ -90,5 +108,45 @@ function reuiredFieldsValidation() {
     } else {
         document.getElementById("calculateBtn").disabled = true;
         // return false; // Any of required field has error
+    }
+}
+
+function savePercantageInputValidation() {
+    const savePercentageInput = document.getElementById("savePercentageInput");
+    const savePercentageInputValue = document.getElementById("savePercentageInput").value;
+    if ( savePercentageInputValue === "" ) {
+        document.getElementById("saveBtn").disabled = true;
+        
+    } else if ( isNaN( savePercentageInputValue ) || savePercentageInputValue < 0 || savePercentageInputValue > 100 ) {
+        document.getElementById( "saveAmountMessage" ).innerText = "Percentage value most be with in 0 - 100!!!";
+        savePercentageInput.style.border = "2px solid #DC3545";
+        document.getElementById("saveBtn").disabled = true;
+    } else {
+        document.getElementById( "saveAmountMessage" ).innerText = "";
+        savePercentageInput.style.border = "";
+        document.getElementById("saveBtn").disabled = false;
+    }
+
+    return;
+}
+
+function totalSavingAmountCalculation() {
+    const savePercentageValue = parseFloat( document.getElementById("savePercentageInput").value );
+    if ( totalBalanceLeft && savePercentageValue ) {
+        const totalSavingAmount = totalBalanceLeft *  savePercentageValue / 100;
+        return document.getElementById("savingAmount").innerText = totalSavingAmount;
+    } else {
+        document.getElementById("savingAmount").innerText = "00";
+        document.getElementById("remainingBalaceAmount").innerText = totalBalanceLeft;
+        return;
+    }
+}
+
+function remainingBalanceCalculation( totalSavingAmount ) {
+    if ( totalBalanceLeft && totalSavingAmount ) {
+        const totalRemainingBalance = totalBalanceLeft - totalSavingAmount;
+        return document.getElementById("remainingBalaceAmount").innerText = totalRemainingBalance;
+    } else if ( totalBalanceLeft ) {
+        document.getElementById("remainingBalaceAmount").innerText = totalBalanceLeft;
     }
 }
